001848e2ff1c24cb14751d6676abcfab3777363a
julio <juliojm1310@gmail.com>
2022-07-31 23:12:15 +0300

    last lesson backend

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-8+df+z5@u9m0py%oixbrdejxr6sfbd53zj8o(+ovegs=u9c258
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -80,13 +80,23 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'todo',
+            'USER': 'admin',
+            'PASSWORD': 'admin123456',
+            'HOST': 'db',
+            'PORT': '5432',
+        }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
